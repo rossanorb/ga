3a2bc39e0c9266032d2827588a93fc92dd9d13b6
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-20 16:30:19 -0300

    Teste de edição de aluno

--- api/tests/Unit/StudentTest.php
+++ api/tests/Unit/StudentTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Student;
 
 class StudentTest extends TestCase
 {
@@ -25,6 +26,8 @@ class StudentTest extends TestCase
 
         // compara http status code e json
         $response->assertStatus(201)->assertJson($param);
+
+        Student::truncate();
     }
 
     public function testUpdateStudent()
@@ -33,5 +36,7 @@ class StudentTest extends TestCase
         $toUpdate = ['email' => 'email_updated@dominio.com.br'];
         $response = $this->json('PUT', '/api/students/'.$student->id, $toUpdate);
         $response->assertStatus(201)->assertJson($toUpdate);
+
+        Student::truncate();
     }
 }
