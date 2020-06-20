a78470cdbb89785108356f2a54e448ad958600dc
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-20 15:57:15 -0300

    Ajuste de teste para novo template de resposta

--- api/tests/Unit/StudentTest.php
+++ api/tests/Unit/StudentTest.php
@@ -10,8 +10,20 @@ class StudentTest extends TestCase
 {
     public function testCreateStudent()
     {
+        // cria um aluno
         $data = factory(\App\Student::class)->make()->toArray();
+
+        // template de resposta da api
+        $param = [
+            'status' => true,
+            'result' =>  $data,
+            'errors' => []
+        ];
+
+        // envia
         $response = $this->json('POST', 'api/students', $data);
-        $response->assertStatus(201)->assertJson($data);
+
+        // compara http status code e json
+        $response->assertStatus(201)->assertJson($param);
     }
 }
