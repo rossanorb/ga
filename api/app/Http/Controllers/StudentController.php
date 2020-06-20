a2584df13e3815387ab7b6a5b227dd24c7c4a423
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-20 15:58:22 -0300

    Cria novo aluno

--- api/app/Http/Controllers/StudentController.php
+++ api/app/Http/Controllers/StudentController.php
@@ -14,7 +14,21 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
+        $request = $request->all();
 
+        try {
+            $this->response['result'] = Student::create($request);
+            $this->response['status'] = true;
+            return $this->response(201);
+
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            array_push($this->response['errors'], [
+                'message' => '( ＾皿＾)っ Something went wrong!',
+                'description' => $e->getMessage()
+            ]);
+            return $this->response(500);
+        }
     }
 
     public function update(Request $request, $id)
