2613b768b3d629eb35e3f8b217029e9eafb20cb1
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-20 17:09:26 -0300

    Edição de aluno

--- api/app/Http/Controllers/StudentController.php
+++ api/app/Http/Controllers/StudentController.php
@@ -33,7 +33,27 @@ class StudentController extends Controller
 
     public function update(Request $request, $id)
     {
+        try {
+            $student = Student::find($id);
+            if(!$student instanceof Student){
+                return $this->response(404);
+            }
 
+            $inputs = $request->only(['name', 'email']);
+            $student->update($inputs);
+            $this->response['status'] = true;
+            $this->response['result'] = $student;
+
+            return $this->response(201);
+
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            array_push($this->response['errors'], [
+                'message' => 'Something went wrong!',
+                'description' => $e->getMessage()
+            ]);
+            return $this->response(500);
+        }
     }
 
     public function destroy($id)
