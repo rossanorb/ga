66f296a84d9d35786ba3093a103dec8b3d6fdd9a
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-21 14:59:43 -0300

    Configuraração store mudules

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import student from './modules/student'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules: {
+        student
+    }
 })
