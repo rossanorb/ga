c3ba2fea9419fd7b51898c939018db501a038501
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-21 12:05:28 -0300

    Definindo template inicial

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,20 @@
 <template>
-    <router-view />
+    <section id="wrapper">
+        <aside id="sidebar">
+            <ul>
+                <li class="user">
+                    <h4>Módulo Acadêmico</h4>
+                </li>
+                <li><a href="">Alunos</a></li>
+            </ul>
+        </aside>
+        <main>
+            <nav>
+                <h3>Admin Template</h3>
+            </nav>
+            <router-view />
+        </main>
+    </section>
 </template>
 
 <style lang="scss">
--- app/src/views/DashBoard.vue
+++ app/src/views/DashBoard.vue
@@ -1,23 +1,7 @@
 <template>
-<!-- Add your site or application content here -->
-<section id="wrapper">
-    <aside id="sidebar">
-        <ul>
-            <li class="user">
-                <h4>Módulo Acadêmico</h4>
-            </li>
-            <li><a href="">Alunos</a></li>
-        </ul>
-    </aside>
-
-    <main id="main">
-        <header id="header">
-            <h3>Admin Template</h3>
-        </header>
-        <div class="container-fluid">
-        </div>
-    </main>
-</section>
+<div class="container-fluid">
+    content here
+</div>
 </template>
 
 <script>
