f09cd6291e84abca0db03b6b0044960dca725937
Rossano Bavaresco <rossanorb@gmail.com>
2020-06-20 22:24:46 -0300

    Definindo componentes

---
+++ app/src/views/DashBoard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
